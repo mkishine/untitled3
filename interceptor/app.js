1d723147849e0c5adcfc0ba4ea5d52194c56310a
mkishine <mkishine@yahoo.com>
2015-12-03 22:04:44 -0500

    Added support for testing running all unit tests in the project

---
+++ interceptor/app.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+var m = angular.module("exampleApp", []);
+m.controller("defaultCtrl", function ($scope, $http) {
+    $http.get("productData.json").success(function (data) {
+        $scope.products = data;
+        $scope.productAttributes = Object.keys($scope.products[0]);
+    });
+});
+
+
+m.config(function ($httpProvider) {
+    // debugger;
+    $httpProvider.interceptors.push(function () {
+        return {
+            response: function (response) {
+                console.log(response);
+                return response;
+            }
+        }
+    });
+});
