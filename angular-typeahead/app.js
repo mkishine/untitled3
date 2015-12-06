224f1fdfbac87fe6de85b4a8cfbbe6b2bba308b1
mkishine <mkishine@yahoo.com>
2015-12-06 08:37:42 -0500

    Simplified input model

--- angular-typeahead/app.js
+++ angular-typeahead/app.js
@@ -1,58 +1,31 @@
 var app = angular.module('plunker', ['siyfion.sfTypeahead']);
 
 app.controller('MainCtrl', function($scope) {
-
-    $scope.selectedNumber = null;
-
-    // instantiate the bloodhound suggestion engine
-    var numbers = new Bloodhound({
-        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.num); },
+    var numbers = ["one", "two", "three", "four", "five"];
+    var numbersBh = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        local: [
-            { num: 'one' },
-            { num: 'two' },
-            { num: 'three' },
-            { num: 'four' },
-            { num: 'five' },
-            { num: 'six' },
-            { num: 'seven' },
-            { num: 'eight' },
-            { num: 'nine' },
-            { num: 'ten' }
-        ]
+        local: numbers
     });
+    function numbersWithDefaults(q, sync) {
+        if (q === '') {
+            sync(numbers);
+        } else {
+            numbersBh.search(q, sync);
+        }
+    }
 
-    // initialize the bloodhound suggestion engine
-    numbers.initialize();
-
+    $scope.selectedNumberNonEditable = null;
     $scope.numbersDataset = {
-        displayKey: 'num',
-        source: numbers.ttAdapter()
+        source: numbersWithDefaults,
     };
-
-    $scope.addValue = function () {
-        numbers.add({
-            num: 'twenty'
-        });
-    };
-
-    $scope.setValue = function () {
-        $scope.selectedNumber = { num: 'seven' };
-    };
-
-    $scope.clearValue = function () {
-        $scope.selectedNumber = null;
-    };
-
-
-    // Typeahead options object
-    $scope.exampleOptions = {
-        highlight: true
-    };
-
     $scope.exampleOptionsNonEditable = {
         highlight: true,
-        editable: false // the new feature
+        minLength: 0,
+        //TODO: figure out why editable:false breaks connection to the model
+        //editable: false,
     };
-
+    $scope.clearValue = function () {
+        $scope.selectedNumberNonEditable = null;
+    };
 });
