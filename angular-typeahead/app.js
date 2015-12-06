fc29ceabdd938a4cd77890053fc90b780637fa80
mkishine <mkishine@yahoo.com>
2015-12-06 09:50:54 -0500

    restored orignal demo, but added default suggestion and scrollable drop-down menu

--- angular-typeahead/app.js
+++ angular-typeahead/app.js
@@ -1,12 +1,28 @@
 var app = angular.module('plunker', ['siyfion.sfTypeahead']);
 
 app.controller('MainCtrl', function($scope) {
-    var numbers = ["one", "two", "three", "four", "five"];
+
+    $scope.selectedNumber = null;
+    $scope.selectedNumberNonEditable = null;
+    // instantiate the bloodhound suggestion engine
+    var numbers = [
+        { num: 'one' },
+        { num: 'two' },
+        { num: 'three' },
+        { num: 'four' },
+        { num: 'five' },
+        { num: 'six' },
+        { num: 'seven' },
+        { num: 'eight' },
+        { num: 'nine' },
+        { num: 'ten' }
+    ];
     var numbersBh = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.num); },
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         local: numbers
     });
+
     function numbersWithDefaults(q, sync) {
         if (q === '') {
             sync(numbers);
@@ -15,17 +31,41 @@ app.controller('MainCtrl', function($scope) {
         }
     }
 
-    $scope.selectedNumberNonEditable = null;
+    // initialize the bloodhound suggestion engine
+    numbersBh.initialize();
+
     $scope.numbersDataset = {
-        source: numbersWithDefaults,
+        displayKey: 'num',
+        source: numbersWithDefaults
     };
-    $scope.exampleOptionsNonEditable = {
-        highlight: true,
-        minLength: 0,
-        //TODO: figure out why editable:false breaks connection to the model
-        //editable: false,
+
+    $scope.addValue = function () {
+        numbers.add({
+            num: 'twenty'
+        });
     };
+
+    $scope.setValue = function () {
+        $scope.selectedNumber = { num: 'seven' };
+        $scope.selectedNumberNonEditable = { num: 'two' };
+    };
+
     $scope.clearValue = function () {
+        $scope.selectedNumber = null;
         $scope.selectedNumberNonEditable = null;
     };
+
+
+    // Typeahead options object
+    $scope.exampleOptions = {
+        highlight: true,
+        minLength: 0,
+    };
+
+    $scope.exampleOptionsNonEditable = {
+        highlight: true,
+        editable: false,
+        minLength: 0,
+    };
+
 });
