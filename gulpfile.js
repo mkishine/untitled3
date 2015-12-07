9102bbc2c25e8331a2ae92560a505b781668a153
mkishine <mkishine@yahoo.com>
2015-12-07 07:28:18 -0500

    experimenting with gulp minify and header injection

--- gulpfile.js
+++ gulpfile.js
@@ -60,3 +60,23 @@ gulp.task('browser-sync', function() {
         logLevel: "debug"
     });
 });
+
+// Minify and header examples are inspired by http://julienrenaux.fr/2014/05/25/introduction-to-gulp-js-with-practical-examples/
+// including plugins
+var minifyHtml = require("gulp-minify-html");
+var header = require("gulp-header");
+
+// Get copyright using NodeJs file system
+var getCopyright = function () {
+    //return fs.readFileSync('Copyright');
+    return "<!-- Hello, Header! -->\n";
+};
+
+// task
+gulp.task('minify-html', function () {
+    gulp.src('./typeahead/*.html') // path to your files
+        .pipe(minifyHtml())
+        .pipe(header(getCopyright()))
+        .pipe(gulp.dest('.tmp'));
+});
+
