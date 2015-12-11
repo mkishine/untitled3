ea5e29935d26a8f18894ccb4d4203e9c1153f602
mkishine <mkishine@yahoo.com>
2015-12-11 06:55:29 -0500

    Added expectations on chart title and data

--- simple-highcharts-directive/directive.spec.js
+++ simple-highcharts-directive/directive.spec.js
@@ -1,20 +1,48 @@
-var Highcharts = {
-    Chart: function(){}
-};
-
 describe("Directive Tests", function () {
     var mockScope;
     var compileService;
+    var ideas = [
+        ['ideas1', 1],
+        ['ideas2', 8]];
+    var scope,
+        options,
+        chart;
     beforeEach(angular.mock.module("myApp"));
     beforeEach(angular.mock.inject(function ($rootScope, $compile) {
         mockScope = $rootScope.$new();
         compileService = $compile;
-        mockScope.limitedIdeas = [
-            ['ideas1', 1],
-            ['ideas2', 8]];
+        mockScope.mockIdeas = ideas;
     }));
+
+
+    beforeEach(inject(function ($injector, $rootScope) {
+        chart = jasmine.createSpyObj('chart', [
+            'redraw',
+            'setTitle',
+            'hideLoading',
+            'destroy',
+            'get',
+            'addSeries']);
+        chart.series = [];
+
+        window.Highcharts = {
+            Chart: function (opt) {
+                options = opt;
+                return chart;
+            },
+        };
+        scope = $rootScope;
+    }));
+
+    afterEach(function () {
+        scope.$destroy();
+    });
+
     it("Generates list elements", function () {
-        var compileFn = compileService('<div class="hc-pie" items="limitedIdeas"></div>');
+        var compileFn = compileService('<div class="hc-pie" items="mockIdeas"></div>');
         var elem = compileFn(mockScope);
+        // console.log(JSON.stringify(options, null, 1));
+        expect(options.title.text).toBe("Browser market shares at a specific website, 2010");
+        expect(options.series[0].data).toEqual(ideas);
     });
 });
