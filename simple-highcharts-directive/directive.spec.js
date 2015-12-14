ed6c134ab357d89416fdd971150e3ab88c993560
mkishine <mkishine@yahoo.com>
2015-12-14 14:37:24 -0500

    [1] Simplifed; [2] Added test for $watch

--- simple-highcharts-directive/directive.spec.js
+++ simple-highcharts-directive/directive.spec.js
@@ -1,48 +1,48 @@
-describe("Directive Tests", function () {
+describe("Simple highchart directive", function () {
+    //mock module
+    beforeEach(angular.mock.module("myApp"));
+
+    // mock scope
     var mockScope;
     var compileService;
     var ideas = [
         ['ideas1', 1],
         ['ideas2', 8]];
-    var scope,
-        options,
-        chart;
-    beforeEach(angular.mock.module("myApp"));
     beforeEach(angular.mock.inject(function ($rootScope, $compile) {
         mockScope = $rootScope.$new();
         compileService = $compile;
         mockScope.mockIdeas = ideas;
     }));
 
-
-    beforeEach(inject(function ($injector, $rootScope) {
-        chart = jasmine.createSpyObj('chart', [
-            'redraw',
-            'setTitle',
-            'hideLoading',
-            'destroy',
-            'get',
-            'addSeries']);
-        chart.series = [];
-
+    // mock highcharts
+    var options, series;
+    beforeEach(function () {
+        series = jasmine.createSpyObj('series', ['setData']);
+        var chart = {
+            series: [series]
+        };
         window.Highcharts = {
             Chart: function (opt) {
                 options = opt;
                 return chart;
             },
         };
-        scope = $rootScope;
-    }));
-
-    afterEach(function () {
-        scope.$destroy();
     });
 
-    it("Generates list elements", function () {
+    it("calls Highcharts.Chart constructor", function () {
         var compileFn = compileService('<div class="hc-pie" items="mockIdeas"></div>');
         var elem = compileFn(mockScope);
         // console.log(JSON.stringify(options, null, 1));
         expect(options.title.text).toBe("Browser market shares at a specific website, 2010");
         expect(options.series[0].data).toEqual(ideas);
+    });
+    // see http://juristr.com/blog/2014/11/learning-ng-testing-watch-expressions/
+    // for more information on testing $watch expressions
+    it("watches changes in data", function () {
+        var compileFn = compileService('<div class="hc-pie" items="mockIdeas"></div>');
+        var elem = compileFn(mockScope);
+        ideas[0][1] = 2;
+        mockScope.$digest();
+        expect(series.setData).toHaveBeenCalledWith(ideas, true);
     });
 });
