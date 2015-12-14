f323f6eb3ea226891335ac5cbe31dc22275520d7
mkishine <mkishine@yahoo.com>
2015-12-14 14:36:47 -0500

    Added support for PhantomJS in karma tests

--- simple-highcharts-directive/karma.config.js
+++ simple-highcharts-directive/karma.config.js
@@ -28,7 +28,6 @@ module.exports = function(config) {
         'highcharts.js'
     ],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
@@ -60,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
