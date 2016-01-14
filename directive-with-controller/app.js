7647bc47934d95e04d56112704fe5cc364b685c9
swglad <sgladstone11@gmail.com>
2016-01-13 20:38:21 -0500

    First directives-with-controller branch commit

--- directive-with-controller/app.js
+++ directive-with-controller/app.js
@@ -1,3 +1,22 @@
 ﻿'use strict';
 
-var m = angular.module("directiveWithControllerApp", []);
+angular.module("directiveWithControllerApp", [])
+    .controller("defaultCtrl", function ($scope) {
+        $scope.products = [{name: "Apples", price: 1.20, quantity: 2},
+            {name: "Bananas", price: 2.42, quantity: 3},
+            {name: "Pears", price: 2.02, quantity: 1}];
+    })
+    .directive("productItem", function () {
+        return {
+            template: document.querySelector("#productTemplate").outerText
+        }
+    })
+    .directive("productTable", function () {
+        return {
+            transclude: true,
+            scope: {
+                value: "=productTable",
+                data: "=productData"
+            }
+        }
+    });
