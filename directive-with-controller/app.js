4c94baea53606da8381b73cbb0a18be439b02663
swglad <sgladstone11@gmail.com>
2016-01-14 11:54:03 -0500

    Add controller to directive

--- directive-with-controller/app.js
+++ directive-with-controller/app.js
@@ -2,13 +2,21 @@
 
 angular.module("directiveWithControllerApp", [])
     .controller("defaultCtrl", function ($scope) {
-        $scope.products = [{name: "Apples", price: 1.20, quantity: 2},
+        $scope.products = [
+            {name: "Apples", price: 1.20, quantity: 2},
             {name: "Bananas", price: 2.42, quantity: 3},
-            {name: "Pears", price: 2.02, quantity: 1}];
+            {name: "Pears", price: 2.02, quantity: 1}
+        ];
     })
     .directive("productItem", function () {
         return {
-            template: document.querySelector("#productTemplate").outerText
+            template: document.querySelector("#productTemplate").outerText,
+            require: "^productTable",
+            link: function(scope, element, attrs, ctrl) {
+                scope.$watch("item.quantity", function() {
+                    ctrl.updateTotal();
+                })
+            }
         }
     })
     .directive("productTable", function () {
@@ -17,6 +25,15 @@ angular.module("directiveWithControllerApp", [])
             scope: {
                 value: "=productTable",
                 data: "=productData"
+            },
+            controller: function($scope, $element, $attrs) {
+                this.updateTotal = function() {
+                    var total = 0;
+                    for (var i = 0; i < $scope.data.length; ++i) {
+                        total += Number($scope.data[i].quantity);
+                    }
+                    $scope.value = total;
+                }
             }
         }
     });
