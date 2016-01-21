ce824c986ab8eba7d4490c0d184dc6069ba3f5eb
mkishine <mkishine@yahoo.com>
2016-01-21 09:01:54 -0500

    Trying to fugure out how to unit test $scope.$watch

---
+++ testing-scope-watch/app.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+(function () {
+    angular.module("exampleApp", [])
+        .controller("defaultCtrl", function ($scope) {
+            var c = this;
+            c.counter = 0;
+            c.anotherCounter = 0;
+            c.incrementCounter = function () {
+                c.counter++;
+            }
+            console.log('setting up watch on ctrl.counter');
+            $scope.$watch('ctrl.counter', function (newValue) {
+                console.log('ctrl.counter updated '+newValue);
+                c.anotherCounter = newValue*2;
+            });
+        })
+    ;
+
+})();
