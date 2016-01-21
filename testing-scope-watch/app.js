f8c57cca4c9a3dd10d1feb0d53207c04633d0d13
mkishine <monday20>
2016-01-21 15:26:28 -0500

    Removed logs

--- testing-scope-watch/app.js
+++ testing-scope-watch/app.js
@@ -7,13 +7,10 @@
             c.anotherCounter = 0;
             c.incrementCounter = function () {
                 c.counter++;
-            }
-            console.log('setting up watch on ctrl.counter');
+            };
             $scope.$watch('ctrl.counter', function (newValue) {
-                console.log('ctrl.counter updated '+newValue);
-                c.anotherCounter = newValue*2;
+                c.anotherCounter = newValue * 2;
             });
         })
     ;
-
 })();
