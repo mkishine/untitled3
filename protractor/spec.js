578b41c2b797499267e405a4f5b0ad4e675a7bfd
mkishine <mkishine@yahoo.com>
2016-02-07 10:47:07 -0500

    Added test for button count

--- protractor/spec.js
+++ protractor/spec.js
@@ -1,11 +1,13 @@
 // timeout notes
 // http://jasmine.github.io/2.3/introduction.html#section-Asynchronous_Support
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10*60*1000;
-
-describe('Protractor Demo App', function() {
+describe('Promises Page', function() {
     it('should have a title', function() {
         browser.get('http://localhost:63342/untitled3/Chapter%2020/Listing%2015.html');
-        browser.pause();
         expect(browser.getTitle()).toEqual('Promises');
+    });
+    it('should have six buttons', function() {
+        browser.get('http://localhost:63342/untitled3/Chapter%2020/Listing%2015.html');
+        expect(element.all(by.tagName('button')).count()).toEqual(6);
     });
 });
