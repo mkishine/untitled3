88cdd579134f6e7e3fb57782a9ece61e989834af
mkishine <monday20>
2016-02-10 18:36:18 -0500

    made an attempt to allow config to be used on both my mac laptop and work PC

--- blob-download/conf.js
+++ blob-download/conf.js
@@ -2,16 +2,19 @@
 exports.config = {
     directConnect: true,
     framework: 'jasmine2',
-
     capabilities: {
-        'browserName': 'chrome',
-        chromeOptions: {
-            binary: 'C:/Program Files (x86)/Google Chrome (Local)/chrome.exe',
-            args: ['--ssl-version-min=tls1', '--ssl-version-fallback-min=tls1', '--no-default-browser-check'],
-            extensions: []
-        }
+        'browserName': 'chrome'
     },
     specs: [
         'spec.js'
     ]
 };
+
+var os = require('os');
+if ( os.platform() === 'win32' ) {
+    exports.config.capabilities.chromeOptions = {
+        binary: 'C:/Program Files (x86)/Google Chrome (Local)/chrome.exe',
+        args: ['--ssl-version-min=tls1', '--ssl-version-fallback-min=tls1', '--no-default-browser-check'],
+        extensions: []
+    };
+}
