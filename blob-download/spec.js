5a68185ad31d585f931c074ecc3b2670201c2e8a
mkishine <monday20>
2016-02-10 17:12:29 -0500

    Added a test for chrome://downloads page

--- blob-download/spec.js
+++ blob-download/spec.js
@@ -1,10 +1,8 @@
 // code for testing content of downloaded file is borrowed from
 // http://stackoverflow.com/questions/21935696
 describe('demo page', function() {
-    beforeEach(function(){
+    it('downloads blob.txt once button is clicked', function(){
         browser.get('http://localhost:63342/untitled3/blob-download');
-    });
-    it('has a button for downloading using blob', function(){
         var filename = '/Users/mkishine/Downloads/blob.txt';
         var fs = require('fs');
         if (fs.existsSync(filename)) {
@@ -17,5 +15,13 @@ describe('demo page', function() {
         }, 30000).then(function() {
             expect(fs.readFileSync(filename, { encoding: 'utf8' })).toEqual('Hello, Blob!');
         });
+    });
+    it('records downloading on chrome://downloads page', function(){
+        browser.driver.get('chrome://downloads/');
+        expect(browser.driver.getTitle()).toEqual('Downloads');
+        var fineElementsPromise = browser.driver.findElements(By.tagName('a'));
+        fineElementsPromise.then(function(a){a[2].getInnerHtml().then(function(txt){
+            expect(txt).toEqual('blob.txt');
+        })});
     })
 });
