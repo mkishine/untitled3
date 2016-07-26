a169d46ae5af6058018a3cb8a5c41b37b0b83c92
mkishine <monday20>
2016-07-26 15:30:49 -0400

    Added instanceof examples

--- polymorphism/app.js
+++ polymorphism/app.js
@@ -5,8 +5,12 @@
         var p1 = new Person("Bob");
         $scope.m1 = p1.sayHello();
         $scope.w1 = p1.walk();
+        $scope.t11 = p1 instanceof Person;
+        $scope.t12 = p1 instanceof Student;
         var p2 = new Student("Fred", "Physics");
         $scope.m2 = p2.sayHello();
         $scope.w2 = p2.walk();
+        $scope.t21 = p2 instanceof Person;
+        $scope.t22 = p2 instanceof Student;
     });
 })();
